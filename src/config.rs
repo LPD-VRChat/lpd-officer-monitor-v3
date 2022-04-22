d3e5f741db28b3c8d24638d352b8245f5a87cbe6
Hrólfur <hrolfurgylfa@protonmail.com>
2022-04-22 14:14:20 +0000

    Setup setting inharitence

--- src/config.rs
+++ src/config.rs
@@ -1,22 +1,22 @@
-use poise::serenity_prelude as serenity;
+use figment::{
+    providers::{Env, Format, Toml},
+    Figment,
+};
 use serde::Deserialize;
 use std::collections::HashSet;
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
 
 #[cfg(not(test))]
 lazy_static! {
-    pub static ref CONFIG: Config = get_config("settings.toml");
+    pub static ref CONFIG: Config = get_config("settings/test.toml", true);
 }
 #[cfg(test)]
 lazy_static! {
-    pub static ref CONFIG: Config = get_config("test_settings.toml");
+    pub static ref CONFIG: Config = get_config("settings/unit_test.toml", false);
 }
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct RoleConfig {
-    pub lpd: serenity::RoleId,
+    pub lpd: u64,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -35,20 +35,49 @@ pub struct Config {
     pub roles: RoleConfig,
     pub patrol_time: PatrolTime,
 }
+/// Calculate the parent folder path with a slash at the end.
+/// Returns an empty string if the path sent in doesn't include any folder and is just a file.
+fn get_parent_folder(file: &str) -> String {
+    match file.rfind("/") {
+        Some(location) => file.split_at(location).0.to_owned() + "/",
+        None => "".to_owned(),
+    }
+}
 
-pub fn get_config(file: &str) -> Config {
-    // Open the file
-    let config_file_path = Path::new(file);
-    let display = config_file_path.display();
-    let mut file = match File::open(&config_file_path) {
-        Err(why) => panic!("Couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
+pub fn get_config(file: &str, include_local: bool) -> Config {
+    let parent_path = get_parent_folder(file);
 
-    // Get the file content
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Unable to read settings file.");
-    // Parse the file as a toml file.
-    toml::from_str(&contents).expect("Error while parsing settings file.")
+    // Get the config
+    let mut figment = Figment::new()
+        .merge(Toml::file(parent_path.clone() + "base.toml"))
+        .merge(Toml::file(file));
+
+    // Only include local config if it is supposed to (don't want it messing with unit testing)
+    if include_local {
+        figment = figment
+            .merge(Toml::file(parent_path + "local.toml"))
+            .merge(Env::prefixed("LOM_"));
+    }
+
+    // Return the resulting config objects
+    let config = figment.extract().expect("Failed to load config file");
+
+    // Show the current configuration
+    println!("Configuration: {:?}", config);
+
+    config
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[rustfmt::skip]
+    fn test_get_parent_folder() {
+        assert_eq!(get_parent_folder("settings.toml"), "".to_owned());
+        assert_eq!(get_parent_folder("/settings.toml"), "/".to_owned());
+        assert_eq!(get_parent_folder("settings/main.toml"), "settings/".to_owned());
+        assert_eq!(get_parent_folder("/settings/main.toml"), "/settings/".to_owned());
+    }
 }
